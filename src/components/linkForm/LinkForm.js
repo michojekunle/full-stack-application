3ecd670289f1772b7fe406c515da668766f5947b
AMD <michojekunle1@gmail.com>
2022-07-29 17:43:45 +0100

    Okay

--- src/components/linkForm/LinkForm.js
+++ src/components/linkForm/LinkForm.js
@@ -1,14 +1,32 @@
 import React from 'react'
-import './LinkForm.css'
+import './LinkForm.css';
+import { useState } from 'react';
 
 const LinkForm= () => {
+  const [input, setInput] = useState('');
+  const [submittedLink, setSubmittedLink] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleInputChange = (e) => {
+    console.log(e.target.value);
+    setInput(e.target.value);
+  }
+  
+  const handleBtnSubmit = () => {
+    console.log('click');
+    setSubmittedLink(input);
+    setSubmitted(true);
+  }
+
   return (
     <div>
       <p className="dark-blue f3 center flex-column">Let's see how many Links / Images you can Submit</p>
       <div className='form center pa4 shadow-5 br3'>
-        <input className='w-70 pa2' type="text" placeholder='Enter Your Link..'/> 
-        <button className='bg-light-purple white pointer w-30 link grow ph4 pv2'>Submit</button>
+        <input className='w-70 pa2' type="text" placeholder='Enter Your Link..' onChange={handleInputChange}/> 
+        <button className='bg-light-purple white pointer w-30 link grow ph4 pv2' onClick={handleBtnSubmit}>Submit</button>
       </div>
+      {submitted && <p className='white f3'>This is your Link: {submittedLink}</p>}
+      
     </div>
   )
 }
--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Rank = () => {
   return (
-    <div>
+    <div >
       <div className='white f3 center flex-column'>Hey, your current rank is</div>
       <div className='white mt3 f1 center flex-column'>#4</div>
     </div>
